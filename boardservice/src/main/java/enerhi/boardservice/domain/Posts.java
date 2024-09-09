2b7796aba7ddcef1af36b95996174392772bf93b
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-09 18:41:02 +0900

    posts id 어노테이션 추가

--- boardservice/src/main/java/enerhi/boardservice/domain/Posts.java
+++ boardservice/src/main/java/enerhi/boardservice/domain/Posts.java
@@ -1,6 +1,8 @@
 package enerhi.boardservice.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
 @Getter @Setter
 public class Posts {
 
+    @Id @GeneratedValue
+    @Column(name = "post_id")
     private Long id;
 
     private String name;
