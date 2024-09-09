908ab97469079d10dee6a76ebdce3559b3743c90
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-09 15:22:13 +0900

    first commit

---
+++ boardservice/src/test/java/enerhi/boardservice/BoardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package enerhi.boardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
