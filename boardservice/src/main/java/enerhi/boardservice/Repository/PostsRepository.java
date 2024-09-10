d830b6079279d258ada98577dc52d62ea9134f6f
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-10 09:22:37 +0900

    Posts 리포지토리 개발

---
+++ boardservice/src/main/java/enerhi/boardservice/Repository/PostsRepository.java
@@ -0,0 +1,23 @@
+package enerhi.boardservice.Repository;
+
+import enerhi.boardservice.domain.Posts;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PostsRepository {
+    private final EntityManager em;
+
+    public void save(Posts post) {
+        em.persist(post);
+    }
+
+    public Posts findOne(Long id) {
+        return em.find(Posts.class, id);
+    }
+
+    
+
+}
