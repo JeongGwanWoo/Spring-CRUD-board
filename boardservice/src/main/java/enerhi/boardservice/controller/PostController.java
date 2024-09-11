cb12b99b77b740183e2a6538fe0a507257b1b50e
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-11 16:41:45 +0900

    Post컨트롤러 추가 - 글 작성, 조회, 도메인 Post폼 추가

---
+++ boardservice/src/main/java/enerhi/boardservice/controller/PostController.java
@@ -0,0 +1,35 @@
+package enerhi.boardservice.controller;
+
+import enerhi.boardservice.domain.PostForm;
+import enerhi.boardservice.domain.Posts;
+import enerhi.boardservice.repository.PostsRepository;
+import enerhi.boardservice.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+    private final PostsService postsService;
+
+    //글 작성
+    @GetMapping("/board/write")
+    public String postWrite(Model model) {
+        model.addAttribute("form", new PostForm());
+        return "redirection:/";
+    }
+
+    //글 목록 조회
+    @GetMapping("/board/list")
+    public String postList(Model model) {
+        List<Posts> posts = postsService.findPosts();
+        model.addAttribute("posts", posts);
+        return "board/postList";
+    }
+}
---
+++ boardservice/src/main/java/enerhi/boardservice/domain/PostForm.java
@@ -0,0 +1,14 @@
+package enerhi.boardservice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PostForm {
+
+    private Long id;
+
+    private String name;
+    private String title;
+    private String content;
+}
