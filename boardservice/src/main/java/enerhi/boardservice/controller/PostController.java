695515f45a3d8dbbe76820119fa780c8103bc91d
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-12 10:04:48 +0900

    글 작성 PostMapping 추가, 모든 layout 변경

--- boardservice/src/main/java/enerhi/boardservice/controller/PostController.java
+++ boardservice/src/main/java/enerhi/boardservice/controller/PostController.java
@@ -1,6 +1,7 @@
 package enerhi.boardservice.controller;
 
 import enerhi.boardservice.domain.PostForm;
+import enerhi.boardservice.domain.PostStatus;
 import enerhi.boardservice.domain.Posts;
 import enerhi.boardservice.repository.PostsRepository;
 import enerhi.boardservice.service.PostsService;
@@ -9,7 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -20,9 +23,25 @@ public class PostController {
 
     //글 작성
     @GetMapping("/board/write")
-    public String postWrite(Model model) {
+    public String postWriteForm(Model model) {
         model.addAttribute("form", new PostForm());
         return "board/write";
+    }
+
+    @PostMapping("/board/write")
+    public String postWrite(PostForm postForm) {
+        Posts post = new Posts();
+        post.setId(postForm.getId());
+        post.setName(postForm.getName());
+        post.setTitle(postForm.getTitle());
+        post.setContent(postForm.getContent());
+        post.setPostDate(LocalDateTime.now());
+        post.setViews(0);
+        post.setStatus(PostStatus.INCLUDE);
+
+        postsService.save(post);
+
+        return "redirect:/board/list";
     }
 
     //글 목록 조회
