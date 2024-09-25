9b43430da01ad3392646f16ff6d3c4c16605ea40
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-25 15:52:47 +0900

    import 정리

--- boardservice/src/main/java/enerhi/boardservice/controller/PostController.java
+++ boardservice/src/main/java/enerhi/boardservice/controller/PostController.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Slf4j
