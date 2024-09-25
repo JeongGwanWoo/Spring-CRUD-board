81906ffd8c33be8b02518269bed5b89121136652
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-09-25 16:28:18 +0900

    db 초기화 수정

--- boardservice/src/test/java/enerhi/boardservice/domain/TestDB.java
+++ boardservice/src/test/java/enerhi/boardservice/domain/TestDB.java
@@ -13,6 +13,16 @@ public class TestDB {
 
     @Test
     void TestDataBase() {
-        Posts post1 =
+
+        for (int i = 0; i < 100; i++) {
+            Posts post = new Posts();
+            post.setName("관우_" + (i + 1));
+            post.setTitle("테스트 " + (i + 1) + "번 입니다.");
+            post.setContent("테스트 내용 " + (i + 1) + "번 입니다.");
+
+            postsService.save(post);
+        }
+
+
     }
 }
